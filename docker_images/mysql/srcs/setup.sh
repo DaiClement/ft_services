34bb3751062f3e186276ceafddf04cb0f2d4af79
Dai Clément <cdai@student.42.fr>
2021-01-13 17:16:48 +0100

    Title:	    Clean nginx and mysql files

--- docker_images/mysql/srcs/setup.sh
+++ docker_images/mysql/srcs/setup.sh
@@ -4,10 +4,10 @@ rc-status --manual;
 rc-status --crashed;
 rc-service mariadb start;
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
-echo "CREATE USER 'cdai'@'%' IDENTIFIED BY 'pass';" | mysql;
-echo "GRANT ALL ON *.* TO 'cdai'@'%' WITH GRANT OPTION;" | mysql;
-echo "CREATE DATABASE wordpress;" | mysql;
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wp-admin'@'%' IDENTIFIED BY 'pass';" | mysql;
-echo "FLUSH PRIVILEGES;" | mysql;
+	echo "CREATE USER 'cdai'@'%' IDENTIFIED BY 'pass';" | mysql;
+	echo "GRANT ALL ON *.* TO 'cdai'@'%' WITH GRANT OPTION;" | mysql;
+	echo "CREATE DATABASE wordpress;" | mysql;
+	echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wp-admin'@'%' IDENTIFIED BY 'pass';" | mysql;
+	echo "FLUSH PRIVILEGES;" | mysql;
 fi
 tail -f /dev/null;
--- docker_images/nginx/srcs/livenessprobe.sh
+++ docker_images/nginx/srcs/livenessprobe.sh
@@ -1,9 +1,6 @@
 #!/bin/ash
 service nginx status
 return_nginx="$?"
-#service sshd status
-#return_sshd=$?
-#if [ $return_nginx = 0 ] && [ $return_sshd = 0 ]; then
 if [ "$return_nginx" = "0" ]; then
 	exit 0
 else
