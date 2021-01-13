3fcd5e57732a6543d652cfbbdbf58779cda0030f
Dai Clément <cdai@student.42.fr>
2021-01-13 20:11:15 +0100

    Title:	    Add livenessprobe.sh for ftps, clean docker image files

---
+++ docker_images/ftps/srcs/livenessprobe.sh
@@ -0,0 +1,8 @@
+#!/bin/ash
+curl --ftp-ssl --insecure --user root:pass ftp://localhost:21
+return_ftps="$?"
+if [ "$return_ftps" = "0" ]; then
+	exit 0
+else
+	exit 1
+fi
--- docker_images/mysql/srcs/setup.sh
+++ docker_images/mysql/srcs/setup.sh
@@ -4,8 +4,8 @@ rc-status --manual;
 rc-status --crashed;
 rc-service mariadb start;
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
-	echo "CREATE USER 'cdai'@'%' IDENTIFIED BY 'pass';" | mysql;
-	echo "GRANT ALL ON *.* TO 'cdai'@'%' WITH GRANT OPTION;" | mysql;
+	echo "CREATE USER 'admin'@'%' IDENTIFIED BY 'pass';" | mysql;
+	echo "GRANT ALL ON *.* TO 'admin'@'%' WITH GRANT OPTION;" | mysql;
 	echo "CREATE DATABASE wordpress;" | mysql;
 	echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wp-admin'@'%' IDENTIFIED BY 'pass';" | mysql;
 	echo "FLUSH PRIVILEGES;" | mysql;
