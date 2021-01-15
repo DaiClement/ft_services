b4558080bf5e4774660209a86bd0760f81593ff6
Dai Clément <cdai@student.42.fr>
2021-01-15 19:13:29 +0100

    Title:		Update yaml_files/setup.sh

--- docker_images/influxdb/srcs/livenessprobe.sh
+++ docker_images/influxdb/srcs/livenessprobe.sh
@@ -1,9 +1,6 @@
 #!/bin/ash
 influx -database 'docker_metrics' -execute 'select * from cpu' -format 'json' -pretty;
 return_influx="$?"
-#service sshd status
-#return_sshd=$?
-#if [ $return_nginx = 0 ] && [ $return_sshd = 0 ]; then
 if [ "$return_influx" = "0" ]; then
 	exit 0
 else
