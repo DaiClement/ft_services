39093830b97b55c69e338141f1d28dc1e976ccdf
Dai Clément <cdai@student.42.fr>
2021-01-15 18:57:36 +0100

    Title:		Update livenessprobe.sh for mysql, wordpress and phpmyadmin

--- docker_images/mysql/srcs/setup.sh
+++ docker_images/mysql/srcs/setup.sh
@@ -1,13 +1,15 @@
-mv tmp/my.cnf /etc/;
+mv /tmp/my.cnf /etc/;
 /usr/bin/mysql_install_db --datadir=/var/lib/mysql;
 rc-status --manual;
 rc-status --crashed;
 rc-service mariadb start;
-if [ ! -d "/var/lib/mysql/wordpress" ]; then
-	echo "CREATE USER 'admin'@'%' IDENTIFIED BY 'pass';" | mysql;
-	echo "GRANT ALL ON *.* TO 'admin'@'%' WITH GRANT OPTION;" | mysql;
-	echo "CREATE DATABASE wordpress;" | mysql;
-	echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wp-admin'@'%' IDENTIFIED BY 'pass';" | mysql;
-	echo "FLUSH PRIVILEGES;" | mysql;
+echo "CREATE USER 'admin'@'%' IDENTIFIED BY 'pass';" | mysql;
+echo "GRANT ALL ON *.* TO 'admin'@'%' WITH GRANT OPTION;" | mysql;
+echo "CREATE DATABASE IF NOT EXISTS wordpress;" | mysql;
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wp-admin'@'%' IDENTIFIED BY 'pass';" | mysql;
+echo "FLUSH PRIVILEGES;" | mysql;
+
+if [ ! -f "/var/lib/mysql/wordpress" ]; then
+	mysql -h localhost wordpress < /tmp/wordpress.sql
 fi
 tail -f /dev/null;
--- docker_images/wp/srcs/livenessprobe.sh
+++ docker_images/wp/srcs/livenessprobe.sh
@@ -1,10 +1,11 @@
 #!/bin/ash
 service nginx status
 return_nginx=$?
-service php-fpm7 status
-return_php=$?
-if [ "$return_nginx" = "0" ] && [ "$return_php" = "0" ]; then
+
+return_php=$(ps | grep 'master process (/etc/php7/php-fpm.conf)' | wc -l)
+if [ "$return_nginx" = "0" ] && [ "$return_php" = "2" ]; then
 	exit 0
 else
+	echo $return_php
 	exit 1
 fi
