265892d4af95d7ed647cc50a0bd54c4bf6320710
cdai <cdai@student.42.fr>
2021-01-21 15:01:30 +0100

    Title:		Update setup.sh to check ram, freespace, and cpus

--- setup.sh
+++ setup.sh
@@ -1,50 +1,18 @@
 #!/usr/bin/env zsh
 
-install()
-{
-	#installation from https://kubernetes.io/docs/tasks/tools/install-kubectl/
-#	echo install kubectl:;
-#	sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2 curl;
-#	curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -;
-#	echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list;
-#	sudo apt-get update;
-#	sudo apt-get install -y kubectl;
-
-	#installation from https://minikube.sigs.k8s.io/docs/start/;
-#	curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb;
-#	sudo dpkg -i minikube_latest_amd64.deb;
-#	rm minikube_latest_amd64.deb;
-
-
-
-	#install filezilla
-#	sudo apt-get install -y filezilla;
-
-	#get metallb manifest
-#	curl https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml > metallb_config.yaml
-#	echo '---' >> metallb_config.yaml
-#	curl https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml >> metallb_config.yaml
-
-	#make docker group and add $USER to docker group. Needed for permission
-	if [[ $(groups | grep docker) = '' ]]
-	then
-		sudo usermod -aG docker $USER
-	fi
-}
-
 launch_ft_services()
 {
-	echo minikube start
 	minikube start --driver=docker --cpus=2 --memory=2000 2>&1 > /dev/null;
 	echo minikube started;
+
 	minikube dashboard 2>&1 > /dev/null &
 	echo launch dashboard;
-#	kubectl apply -f metallb_config.yaml;
+
 	kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.8.1/manifests/metallb.yaml > /dev/null
 	kubectl apply -f ./yaml_files/metallb_layer2.yaml > /dev/null ;
 	echo setup metallb;
+
 	minikube addons enable metrics-server > /dev/null ;
-#	minikube addons enable metallb;
 	eval $(minikube -p minikube docker-env) > /dev/null ;
 
 	docker build -t nginx-alpine ./docker_images/nginx/ > /dev/null ; 
@@ -77,14 +45,32 @@ launch_ft_services()
 
 }
 
-echo $(grep MemTotal /proc/meminfo | awk '{print $2}') bytes of ram '|' Need 2Go of ram;
-echo $(grep 'cpu cores' /proc/cpuinfo | uniq | awk '{print $4}') cpus '|' Need 2 free cpus;
+memTotal=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+nbCpus=$(grep 'cpu cores' /proc/cpuinfo | uniq | awk '{print $4}')
+freeSpace=$(expr $(df | grep /dev/sda1 | awk '{print $4}'))
+
+echo $memTotal bytes ram memory
+echo $nbCpus cpus
+echo $freeSpace bytes of free spaces
+
+if [ $memTotal -le 4000000 ]; then
+	echo Not enough RAM
+	exit 1
+elif [ $nbCpus -lt 4 ]; then
+	echo Not enough cpus
+	exit 1
+elif [ $freeSpace -le 5000000 ]; then
+	echo Not enough free spaces
+	exit 1
+fi
+
 if [[ $(groups | grep docker) = '' ]]
 then
 	sudo usermod -aG docker $USER
 	echo "close session and reconnect to update 'docker' group"
 	exit
 fi
+
 if [ "$1" = "re" ]
 then
 	minikube stop > /dev/null ;
@@ -95,4 +81,5 @@ then
 	launch_ft_services;
 else
 	echo \"$1\" is not an argument
+	exit 1
 fi
